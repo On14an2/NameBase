7c1205c73b8e21c4c4a582e9267912e283b98cc0
on14an <112744129+On14an2@users.noreply.github.com>
2023-06-08 12:02:40 +0700

    Add files via upload

---
+++ NameBase/NameBase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> names = new List<string>();
+
+        Console.WriteLine("Введите имена (для завершения введите пустую строку):");
+
+        while (true)
+        {
+            string name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name) || name.Trim().Length == 0)
+                break;
+
+            names.Add(name);
+        }
+
+        Console.WriteLine("Введите имя для поиска:");
+        string searchName = Console.ReadLine();
+
+        if (names.Contains(searchName))
+        {
+            Console.WriteLine($"Имя {searchName} найдено в списке.");
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+
+        Console.ReadLine();
+    }
+}
